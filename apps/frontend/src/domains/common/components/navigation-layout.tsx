2b45bb2a382d4e25cc98cf0003d395390b2114c2
freddie <rincofrederico@gmail.com>
2023-09-07 19:48:08 +0100

    fix: tab bar at bottom

--- apps/frontend/src/domains/common/components/navigation-layout.tsx
+++ apps/frontend/src/domains/common/components/navigation-layout.tsx
@@ -15,32 +15,34 @@ export const NavigationLayout = () => {
 	return (
 		<>
 			<Navbar user={user} />
-			<Tabbar.Container className="flex-shrink-0">
-				<Link className="flex flex-1" to="/">
-					<Tabbar.Item isActive={pathname === '/'} icon={{ active: <IoFlame />, inactive: <IoFlameOutline /> }}>
-						Matches
-					</Tabbar.Item>
-				</Link>
-				<div className="flex -translate-y-1/3 items-center">
-					<Button size="unsized" asChild>
-						<Link className="aspect-square flex-1 px-4 py-2" to="/join-match">
-							<div className="flex flex-col items-center gap-0.5">
-								<IoFlame className="label-lg" />
-								<span className="label-xs">Join</span>
-							</div>
-						</Link>
-					</Button>
-				</div>
+			<div className="fixed bottom-0 w-full">
+				<Tabbar.Container className="flex-shrink-0">
+					<Link className="flex flex-1" to="/">
+						<Tabbar.Item isActive={pathname === '/'} icon={{ active: <IoFlame />, inactive: <IoFlameOutline /> }}>
+							Matches
+						</Tabbar.Item>
+					</Link>
+					<div className="flex -translate-y-1/3 items-center">
+						<Button size="unsized" asChild>
+							<Link className="aspect-square flex-1 px-4 py-2" to="/join-match">
+								<div className="flex flex-col items-center gap-0.5">
+									<IoFlame className="label-lg" />
+									<span className="label-xs">Join</span>
+								</div>
+							</Link>
+						</Button>
+					</div>
 
-				<Link className="flex flex-1" to="/users">
-					<Tabbar.Item
-						isActive={pathname.includes('/users')}
-						icon={{ active: <IoPersonCircle />, inactive: <IoPersonCircleOutline /> }}
-					>
-						Users
-					</Tabbar.Item>
-				</Link>
-			</Tabbar.Container>
+					<Link className="flex flex-1" to="/users">
+						<Tabbar.Item
+							isActive={pathname.includes('/users')}
+							icon={{ active: <IoPersonCircle />, inactive: <IoPersonCircleOutline /> }}
+						>
+							Users
+						</Tabbar.Item>
+					</Link>
+				</Tabbar.Container>
+			</div>
 			<div className="flex flex-grow flex-col pb-safe-bottom pt-safe-top">
 				<main className="flex flex-grow flex-col py-16">
 					<Outlet />
